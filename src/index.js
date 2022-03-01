9c1456282795b84c3f93337dbed6eb032276deb0
John McCollum <john.mccollum@arnoldclark.com>
2022-03-01 10:52:49 +0000

    Add mount node so that app isn't mounted on <body>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-var mountNode = document.querySelector("body");
+const body = document.querySelector("body");
+const mountNode = document.createElement("div");
+body.appendChild(mountNode);
+
 ReactDOM.render(<App />, mountNode);
