7be3230a6007a32ac21ace49c25b59a2ea01da85
John McCollum <john.mccollum@arnoldclark.com>
2022-03-11 11:35:57 +0000

    Omit test files from ts-convert + allow synthetic default imports

--- scripts/convert-ts.js
+++ scripts/convert-ts.js
@@ -12,7 +12,7 @@ const listDir = (dir, fileList = []) => {
     if (fs.statSync(path.join(dir, file)).isDirectory()) {
       fileList = listDir(path.join(dir, file), fileList);
     } else {
-      if (/\.js$/.test(file) && !/\.test\.$/.test(file)) {
+      if (/\.js$/.test(file) && !/\.test\./.test(file)) {
         let name = file.split(".")[0].replace(/\s/g, "_") + ".tsx";
         let src = path.join(dir, file);
         let newSrc = path.join(dir, name);
