d299a35a3baab48c00145725681657a53710bee0
John McCollum <john.mccollum@arnoldclark.com>
2022-08-23 09:12:27 +0100

    Adds index.html template

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const container = document.createElement("div", { id: "app" });
-document.body.append(container);
+const container = document.getElementById("app");
 
 const root = createRoot(container);
 root.render(<App />);
